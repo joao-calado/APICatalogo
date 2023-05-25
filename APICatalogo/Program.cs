64ae3dfbbdef341367afe2b8af31dc49d5415a61
João Calado <joao1823@hotmail.com>
2023-05-25 20:58:50 -0300

    Serialização e Desserialização JSON. Otimizando consultas GET. Fazendo tratamento de erros.

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,9 +1,13 @@
 using APICatalogo.Context;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers().AddJsonOptions(options => 
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
