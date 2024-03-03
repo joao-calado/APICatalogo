5b85d64b1edafe911217b78efe64008ca2ff7d2c
joao-calado <joaovsscalado@gmail.com>
2024-03-03 17:16:26 -0300

    Utilizando Data Annotations para validar modelo.

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -10,20 +10,21 @@ public class Produto
     [Key]
     public int ProdutoId { get; set; }
 
-    [Required]
-    [StringLength(80)]
+    [Required(ErrorMessage = "O nome é obrigatório")]
+    [StringLength(20, MinimumLength = 5, ErrorMessage = "O nome deve ter entre 5 e 20 caracteres")]
     public string? Nome { get; set; }
 
     [Required]
-    [StringLength(300)]
+    [StringLength(10, ErrorMessage = "A descrição deve ter no máximo {1} caracteres")]
     public string? Descricao { get; set; }
 
     [Required]
     [Column(TypeName = "decimal(10,2)")]
+    [Range(1,10000, ErrorMessage = "O preço deve estar entre {1} e {2}")]
     public decimal Preco { get; set; }
 
     [Required]
-    [StringLength(300)]
+    [StringLength(300, MinimumLength = 10)]
     public string? ImagemUrl { get; set; }
 
     public float Estoque { get; set; }
