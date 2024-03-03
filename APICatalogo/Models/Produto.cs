ccda8c0a0adefe0573e936916397f861c28b6798
joao-calado <joaovsscalado@gmail.com>
2024-03-03 20:33:01 -0300

    Validação Personalizada

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using APICatalogo.Validations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
@@ -12,6 +13,7 @@ public class Produto
 
     [Required(ErrorMessage = "O nome é obrigatório")]
     [StringLength(20, MinimumLength = 5, ErrorMessage = "O nome deve ter entre 5 e 20 caracteres")]
+    [PrimeiraLetraMaiuscula]
     public string? Nome { get; set; }
 
     [Required]
