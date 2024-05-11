3d193cb2075bebf414605d9ac0a7a419f9746808
joao-calado <joaovsscalado@gmail.com>
2024-05-11 16:53:39 -0300

    Implementando a geração de token

---
+++ APICatalogo/DTOs/RegisterModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICatalogo.DTOs;
+
+public class RegisterModel
+{
+    [Required(ErrorMessage ="User name is required")]
+    public string? UserName { get; set; }
+
+    [EmailAddress]
+    public string? Email { get; set; }
+
+    [Required(ErrorMessage ="Password is required")]
+    public string? Password { get; set; }
+}
