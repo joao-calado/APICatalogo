74a8666ecde32b9b29751db9758ad5453f533465
joao-calado <joaovsscalado@gmail.com>
2024-05-30 19:11:27 -0300

    feat: inclusão de endpoints para criar "role" e adicionar usuário à uma role.

--- APICatalogo/Controllers/AuthController.cs
+++ APICatalogo/Controllers/AuthController.cs
@@ -18,17 +18,92 @@ public class AuthController : ControllerBase
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly RoleManager<IdentityRole> _roleManager;
     private readonly IConfiguration _configuration;
+    private readonly ILogger<AuthController> _logger;
 
-    public AuthController(ITokenService tokenService, 
-                          UserManager<ApplicationUser> userManager, 
-                          RoleManager<IdentityRole> roleManager, 
-                          IConfiguration configuration)
+    public AuthController(ITokenService tokenService,
+                          UserManager<ApplicationUser> userManager,
+                          RoleManager<IdentityRole> roleManager,
+                          IConfiguration configuration,
+                          ILogger<AuthController> logger)
     {
         _tokenService = tokenService;
         _userManager = userManager;
         _roleManager = roleManager;
         _configuration = configuration;
+        _logger = logger;
     }
+
+    [HttpPost]
+    [Route("CreateRole")]
+    public async Task<IActionResult> CreateRole(string roleName)
+    {
+        var roleExist = await _roleManager.RoleExistsAsync(roleName);
+
+        if (!roleExist)
+        {
+            var roleResult = await _roleManager.CreateAsync(new IdentityRole(roleName));
+
+            if (roleResult.Succeeded)
+            {
+                _logger.LogInformation(1, "Roles Added");
+                return StatusCode(StatusCodes.Status200OK,
+                                  new Response
+                                  {
+                                      Status = "Success",
+                                      Message = $"Role '{roleName}' added successfully"
+                                  });
+            }
+            else
+            {
+                _logger.LogInformation(2, "Error");
+                return StatusCode(StatusCodes.Status400BadRequest,
+                                  new Response
+                                  {
+                                      Status = "Error",
+                                      Message = $"Issue adding the new {roleName} role"
+                                  });
+            }
+        }
+        return StatusCode(StatusCodes.Status400BadRequest,
+                          new Response
+                          {
+                              Status = "Error",
+                              Message = "Role already exist"
+                          });
+    }
+
+    [HttpPost]
+    [Route("AddUserToRole")]
+    public async Task<IActionResult> AddUserToRole(string email, string roleName)
+    {
+        var user = await _userManager.FindByEmailAsync(email);
+        if (user != null)
+        {
+            var result = await _userManager.AddToRoleAsync(user, roleName);
+            if (result.Succeeded)
+            {
+                _logger.LogInformation(1, $"User '{user.Email}' added to the '{roleName}' role");
+                return StatusCode(StatusCodes.Status200OK,
+                                  new Response
+                                  {
+                                      Status = "Success",
+                                      Message = $"User '{user.Email}' added to the '{roleName}' role"
+                                  });
+            }
+            else
+            {
+                _logger.LogInformation(1, $"Error: unable to add user '{user.Email}' to the '{roleName}' role");
+                return StatusCode(StatusCodes.Status400BadRequest,
+                                  new Response
+                                  {
+                                      Status = "Error",
+                                      Message = $"Error: unable to add user '{user.Email}' to the '{roleName}' role"
+                                  });
+            }
+        }
+        return BadRequest(new { error = "Unable to find user" });
+    }
+
 
     [HttpPost]
     [Route("login")]
